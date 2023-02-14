af1dc5e85808d0ecc34964633b9acfaba4d6b656
superchangoman2 <40414462+superchangoman2@users.noreply.github.com>
2023-02-13 23:42:49 -0600

    2da Pre-entrega

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Inicio extends Component {
   render() {
     return (
-      <div><h1>Inicio</h1></div>
+      <div><h1>Inicio </h1></div>
     )
   }
 }
